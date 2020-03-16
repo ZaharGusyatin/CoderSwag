2603cb352ffa83f1d8a61d4c2664fc0717e06ad7
Zahar <redimerlives@gmail.com>
2020-03-16 20:35:51 +0200

    hru

---
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.coderswag.Controller
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.coderswag.Adapters.CategoryAdapter
+import com.example.coderswag.Adapters.CategoryRecycleAdapter
+import com.example.coderswag.Model.Category
+import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
+import com.example.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+        private lateinit var adapter : CategoryRecycleAdapter
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            adapter = CategoryRecycleAdapter(this, DataService.categories){
+                category ->  val productIntent=Intent(this, ProductsActivity::class.java)
+                productIntent.putExtra(EXTRA_CATEGORY,category.title)
+                startActivity(productIntent)
+            }
+            categoryListView.adapter = adapter
+
+
+            val layoutManager = LinearLayoutManager(this)
+            categoryListView.layoutManager = layoutManager
+            categoryListView.setHasFixedSize(true)
+        }
+    }
